f523d05c2f5c574bb82d2bb79864e25fab01fed9
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2019-08-02 17:30:08 +0300

    add class to display none

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
 const menu = document.querySelector("#hamburger");
 const page = document.querySelector('body');
+const hideMe = document.querySelector('.container');
 
 menu.addEventListener('click', function () {
    
     if (menu.classList.contains('active')) {
         menu.classList.remove('active');
         page.classList.remove('page-active');
+        hideMe.classList.remove('testId');
     } else {
         menu.classList.add('active');
         page.classList.add('page-active');
+        hideMe.classList.add('testId');
     }
 
     console.log()
