8626d2e453107355f5122cb82c06e66566eb75f0
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2019-08-02 20:11:57 +0300

    add variables to display hidden class

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector("#hamburger");
 const page = document.querySelector('body');
-const hideMe = document.querySelector('.container');
+const hideMe = document.querySelector('.show');
+const nav = document.querySelector('nav');
+const para = document.querySelector('#secondPara')
 
 menu.addEventListener('click', function () {
    
@@ -8,10 +10,14 @@ menu.addEventListener('click', function () {
         menu.classList.remove('active');
         page.classList.remove('page-active');
         hideMe.classList.remove('testId');
+        nav.classList.remove('navigator');
+        para.classList.add('hideMe');
     } else {
         menu.classList.add('active');
         page.classList.add('page-active');
         hideMe.classList.add('testId');
+        nav.classList.add('navigator')
+        para.classList.remove('hideMe');
     }
 
     console.log()
